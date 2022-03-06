63d99045594afcd75beaa227eaabb60b88c33030
Cristian Sánchez <christian.sanchez@kinesso.com>
2022-03-06 16:54:15 -0500

    Added the endpoints not related to sensible information

--- password-api/src/main/java/co/crisi/controller/AccountController.java
+++ password-api/src/main/java/co/crisi/controller/AccountController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import co.crisi.application.service.AccountApplicationService;
 import co.crisi.domain.Account;
 
@@ -26,5 +23,31 @@ public class AccountController {
         boolean isAdded = accountApplicationService.add(account);
         return new ResponseEntity<>(isAdded, HttpStatus.CREATED);
     }
+
+    @DeleteMapping
+    public ResponseEntity<Boolean> deleteAccount(@RequestBody Account account){
+        boolean idDeleted = accountApplicationService.delete(account);
+        return new ResponseEntity<>(idDeleted, HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Boolean> deleteById(@RequestParam(name = "id") Long id){
+        boolean isDeleted = accountApplicationService.deleteById(id);
+        return new ResponseEntity<>(isDeleted, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping(path = "/{ids}")
+    public ResponseEntity<HttpStatus> deleteAllById(@PathVariable List<Long> ids){
+        accountApplicationService.deleteAllById(ids);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(path = "/exists/{id}")
+    public ResponseEntity<Boolean> existsById(@PathVariable Long id){
+        boolean exists = accountApplicationService.existsById(id);
+        return new ResponseEntity<>(exists, HttpStatus.OK);
+    }
+
 
 }
